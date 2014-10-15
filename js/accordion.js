edbc273d83093ad3c37e3402acc25da8a062511f
Preetham Madapura <mnpreetham@gmail.com>
2014-10-15 11:22:46 -0700

    Added CSS toggle icons
    - Made changes in CSS for toggle icons to work.
    - Deleted the accordion.js file.
    - Some changes in HTML

--- js/accordion.js
+++
@@ -1,7 +0,0 @@
-
-/*jquery code to toggle icons in accordion*/
-$('.collapse').on('shown.bs.collapse', function(){
-$(this).parent().find(".glyphicon-plus").removeClass("glyphicon-plus").addClass("glyphicon-minus");
-}).on('hidden.bs.collapse', function(){
-$(this).parent().find(".glyphicon-minus").removeClass("glyphicon-minus").addClass("glyphicon-plus");
-});
