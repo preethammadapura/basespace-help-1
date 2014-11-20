803a941135a6bfb1516e60b84e7006df7cc8dbcf
Preetham Madapura <mnpreetham@gmail.com>
2014-11-20 14:17:14 -0800

    burger icon added and linked to sidenav
    fully functional burger icon with toggle functionality added

--- js/navigation.js
+++ js/navigation.js
@@ -2,3 +2,11 @@
     target: '.bs-docs-sidebar',
     offset: 240
   });
+
+  $('.menu-box').click(function() {
+  	$( this ).toggleClass("menu-box-selected");
+  	$('.vertical-line').toggleClass("vertical-line-toggle");
+  	$('#sidenav').slideToggle("slow");
+  });
+
+ 
