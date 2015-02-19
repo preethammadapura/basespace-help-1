a567a1a2de884e46cc62b8f06bb1345fccd0166e
aroberts-illumina <aroberts@illumina.com>
2015-02-19 12:42:18 -0800

    Re-work layout and responsive styles

--- js/navigation.js
+++ js/navigation.js
@@ -1,12 +1,4 @@
-  $('body').scrollspy({
-    target: '.bs-docs-sidebar',
-    offset: 240
-  });
-
-  $('.menu-box').click(function() {
-  	$( this ).toggleClass("menu-box-selected");
-  	$('.vertical-line').toggleClass("vertical-line-toggle");
-  	$('#sidenav').slideToggle("fast");
-  });
-
- 
+$('#sidenav-toggle').click(function(e) {
+	e.preventDefault();
+	$("#sidenav-list").toggle();
+}); 
