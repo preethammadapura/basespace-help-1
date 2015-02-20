86f360b23c541c0cf735bfbccffdafc307821e76
Preetham Madapura <mnpreetham@gmail.com>
2015-02-20 12:53:30 -0800

    finished article page, breadcrumb and accordion page

--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,7 @@
 $('#sidenav-toggle').click(function(e) {
 	e.preventDefault();
-	$("#sidenav-list").toggle();
+	$("#sidenav-list").slideToggle("fast");
+	$('.menu-box').toggleClass("menu-box-selected");
+	$('.menu-icon').toggleClass("menu-icon-selected");
+  	$('.vertical-line').toggleClass("vertical-line-toggle");
 }); 
