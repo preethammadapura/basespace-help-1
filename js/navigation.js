a747d3e335ef66b1168c2af1f37713c2c5336635
Preetham Madapura <mnpreetham@gmail.com>
2015-02-23 17:34:30 -0800

    set anchors on new to basespace and fix other minor issues

--- js/navigation.js
+++ js/navigation.js
@@ -1,7 +1,10 @@
-$('#sidenav-toggle').click(function(e) {
-	e.preventDefault();
-	$("#sidenav-list").slideToggle("fast");
-	$('.menu-box').toggleClass("menu-box-selected");
-	$('.menu-icon').toggleClass("menu-icon-selected");
-  	$('.vertical-line').toggleClass("vertical-line-toggle");
-}); 
+$(function() { 
+	$('#sidenav-toggle').click(function(e) {
+		console.log("Click~!");
+		e.preventDefault();
+		$("#sidenav-list").slideToggle("fast");
+		$('.menu-box').toggleClass("menu-box-selected");
+		$('.menu-icon').toggleClass("menu-icon-selected");
+	  	$('.vertical-line').toggleClass("vertical-line-toggle");
+	}); 
+});
